cb2ead78eb97334206b3bef848d3bd5d14ba6359
Marco Z <mar.marcoz@outlook.com>
2017-09-22 19:51:25 -0400

    Initial

---
+++ main.py
@@ -0,0 +1,118 @@
+import argparse
+import os
+import data_to_ground as d2g
+import predictions_to_pred as p2p
+import evaluate_pred_ground as epg
+import matplotlib.pyplot as plt
+
+def main(predict_dir, truth_dir \
+        , data_dir = None \
+        , predictions_file = None \
+        , start_point = None \
+        , end_point = None \
+        # , truth_save_dir = None \
+        , skip_reverse = False \
+        , mode = [] \
+        , suppress_display = False \
+        , output_files = False \
+        , output_name = None \
+        , price_col = 3 \
+        , new_price_row = 1 \
+        , old_price_row = 0 \
+        , prediction_col = 1 \
+        , weight_up_col = 4 \
+        , weight_down_col = 5 \
+):
+    if os.path.isdir(data_dir): # process data
+        d2g.get_truth_from_data(data_dir = data_dir, skip_reverse = skip_reverse, price_col = price_col, new_offset = new_price_row, old_offset = old_price_row, save_dir = truth_dir, get_dataframe=False)
+
+    if os.path.isfile(predictions_file): # process predictions
+        p2p.get_predictions_from_file(predictions_file, skip_reverse = skip_reverse, col_prediction = prediction_col, col_voteup = weight_up_col, col_votedown = weight_down_col, save_dir = predict_dir, get_dataframe = False)
+
+    truth_classes, predicted_classes, scores = epg.get_classes_scores_from_dir(predict_dir, truth_dir, start_point = start_point, end_point = end_point, skip_reverse = skip_reverse)
+
+    epg.get_metric_plots(truth_classes, predicted_classes, scores, mode = [], output_file = output_files, output_live = not suppress_display)
+
+    print('Finished')
+
+def get_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='''Generate machine learning metrics from Genotick predictions.
+You need a predictions_x.csv and the original data/ directory.
+
+Suggested use, first time (to generate market readings):
+
+    genometrics "./predict" "./truth" --data "./data" 
+        --predictions "predictions_x.csv" --start 20170101 
+        --skipreverse --outputfiles
+
+Suggested use, with already-generated market readings:
+
+    genometrics "./predict" "./truth" --start 20170101 
+        --skipreverse --outputfiles
+
+''', epilog='''Column and row defaults (--pricecol, --newpricerow, etc.) should already work
+for Genotick. Use these settings only if you have differently formatted CSV files.''')
+    parser.add_argument('predictdir', type=str
+        , help='Directory with predicted market readings. If --predictions is passed, generate and save the readings to this dir. Else, read the readings.')
+    parser.add_argument('truthdir', type=str
+        , help='Directory with actual market readings. If --data is passed, generate and save the readings. Else, read the readings.')
+    parser.add_argument('--data', '-d', type=str, default=None
+        , help='Data directory to generate market readings.')
+    parser.add_argument('--predictions', '-c', type=str, default=None
+        , help='File to generate predicted market readings.')
+    parser.add_argument('--start', '-s', type=int, default=None
+        , help='Starting TimePoint, inclusive. default: earliest')
+    parser.add_argument('--end', '-e', type=int, default=None
+        , help='Ending TimePoint, inclusive. default: latest')
+    # parser.add_argument('--readtruth', '-t', type=str, default=None
+    #     , help='Directory to read actual market readings. If this is specified, program will use these readings instead of generating them from data. Expected column layout is TimePoint,Direction[,Delta (is ignored)].')
+    # parser.add_argument('--savetruth', '-s', type=str, default=None
+    #     , help='Specify a directory if you want to save actual market readings.')
+    # parser.add_argument('--savepredicted', '-c', type=str, default=None
+    #     , help='Specify a directory if you want to save predicted market readings.')
+    parser.add_argument('--skipreverse', '-k', action='store_true', default=False
+        , help='Skip reverse data.')
+    parser.add_argument('--mode', '-m', type=str, nargs='+', default=[]
+        , help='List of metrics to process, space-separated. choices: confusion, roc. default: all')
+    parser.add_argument('--suppressdisplay', '-b', action='store_true', default=False
+        , help='Do not display evaluation metrics upon completion')
+    parser.add_argument('--outputfiles', '-o', action='store_true', default=False
+        , help='Output evaluation metrics as image files')
+    parser.add_argument('--pricecol', '-p', type=int, default=3
+        , help='In data files, price column index to determine market direction. Index does not include TimePoint column (i.e., open = 0). default: 3 (close)')
+    parser.add_argument('--newpricerow', '-n', type=int, default=1
+        , help='In data files, new price row to determine market direction, relative to TimePoint. default: 1 (next future TimePoint)')
+    parser.add_argument('--oldpricerow', '-l', type=int, default=0
+        , help='In data files, old price row to determine market direction, relative to TimePoint. default: 0 (current TimePoint)')
+    parser.add_argument('--predictioncol', '-r', type=int, default=1
+        , help='In prediction file, prediction column index. Index does not include TimePoint column. default: 1')
+    parser.add_argument('--weightupcol', '-u', type=int, default=4
+        , help='In prediction file, vote weight up column index. Index does not include TimePoint column. default: 4')
+    parser.add_argument('--weightdowncol', '-w', type=int, default=5
+        , help='In prediction file, vote weight up column index. Index does not include TimePoint column. default: 5')
+    
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args.predictdir, args.truthdir \
+        , data_dir = args.data \
+        , predictions_file = args.predictions \
+        , start_point = args.start \
+        , end_point = args.end \
+        # , truth_dir = args.readtruth \
+        # , truth_save_dir = args.savetruth \
+        # , predicted_save_dir = args.savepredicted \
+        , skip_reverse = args.skipreverse \
+        , mode = args.mode \
+        , suppress_display = args.suppressdisplay \
+        , output_files = args.outputfiles \
+        # output_name = None
+        , price_col = args.pricecol \
+        , new_price_row = args.newpricerow \
+        , old_price_row = args.oldpricerow \
+        , prediction_col = args.predictioncol \
+        , weight_up_col = args.weightupcol \
+        , weight_down_col = args.weightdowncol
+    )
---
+++ predictions_to_pred.py
@@ -0,0 +1,99 @@
+import os
+import csv
+import argparse
+import pandas
+
+def main(working_dir='.', score_type='weight'):
+    for fn in os.listdir(working_dir):
+        if os.path.isfile(os.path.join(working_dir, fn)):
+            fn_type, fn_name = get_fn_pieces(fn)
+            if (fn_type == 'predictions' or fn_type == 'prediction') and 'csv' in fn:
+                print('Processing {}'.format(fn))
+                get_predictions_from_file(fn, working_dir=working_dir, score_type=score_type)
+    print('Finished')
+
+def get_predictions_from_file(fn, skip_reverse = False, col_prediction = 1, col_voteup = 4, col_votedown = 5, save_dir = None, score_by_positive_class = True, get_dataframe = True, format_is_tf_inst_time = False):
+    col_prediction, col_voteup, col_votedown = col_prediction+1, col_voteup+1, col_votedown+1 # add for TimePoint
+    if format_is_tf_inst_time: col_tf, col_inst, col_date = 0, 1, 2
+    else: col_date, col_name = 0, 1
+    save_output = not save_dir is None
+    dest_files = {}
+    output = {}
+
+    if save_output: os.makedirs(save_dir, exist_ok=True)
+
+    with open(fn, 'r') as fr:
+        csvr = csv.reader(fr)
+        for row in csvr:
+            name = '{}_{}.csv'.format(row[col_inst], row[col_tf]) if format_is_tf_inst_time else os.path.basename(row[col_name])
+            if skip_reverse and name.lower().startswith('reverse_'): continue
+
+            if save_output:
+                dest_fn = os.path.join(save_dir, name)
+                if dest_fn in dest_files: dest_fw = dest_files[dest_fn]
+                else:
+                    dest_files[dest_fn] = open(dest_fn, 'w', newline='\n')
+                    dest_fw = dest_files[dest_fn]
+                csvw = csv.writer(dest_fw)
+
+            if get_dataframe: 
+                if not name in output: output[name] = pandas.DataFrame(data=None, columns=['Prediction','Score'])
+
+            prediction = prediction_to_numeric(row[col_prediction])
+            if prediction == 0: score = 0
+            else:
+                col_vote = col_votedown if not score_by_positive_class and prediction < 0 else col_voteup
+                score_total = float(row[col_votedown]) + float(row[col_voteup])
+                score = float(row[col_vote]) / score_total
+
+            row = [
+                row[col_date]
+                , prediction
+                , score
+            ]
+
+            if save_output: csvw.writerow(row)
+            if get_dataframe: output[name] = output[name].append(pandas.DataFrame([row[1:]], index=[row[0]], columns=['Prediction','Score']))
+
+    if save_output: close_file_dict(dest_files)
+    if get_dataframe: return output
+
+def prediction_to_numeric(prediction):
+    if prediction.isdigit(): return prediction
+    elif prediction.upper() == 'UP': return 1
+    elif prediction.upper() == 'DOWN': return -1
+    else: return 0
+
+def close_file_dict(file_dict):
+    names = list(file_dict.keys())
+
+    for fn in names:
+        file_dict.pop(fn, None).close()
+
+def get_fn_pieces(fn):
+    pieces = fn.split('_', 1)
+    return pieces[0], pieces[1] if len(pieces) > 1 else None
+
+def read_predictions_from_dir(prediction_path, skip_reverse = False):
+    output = {}
+
+    for fn in os.listdir(prediction_path):
+        if skip_reverse and fn.lower().startswith('reverse_'): continue
+        output[fn] = pandas.read_csv(os.path.join(truth_dir, fn), header=None, names=['TimePoint', 'Prediction', 'Score'], index_col=0)
+
+    return output
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('--col', '-c', type=int, default=0
+    #    , help='Prediction column starting after date')
+    parser.add_argument('--scoretype', '-t', type=str, default='weight_up'
+        , help='Score method: weight, count, weight_up, weight_down, count_up, count_down')
+    #parser.add_argument('--scoreoffset', '-k', type=int, default=6)
+    parser.add_argument('--workingdir', '-w', type=str, default='.')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_args()
+    main(working_dir=args.workingdir, score_type=args.scoretype)
+#       , prediction_col=args.col, score_offset=args.scoreoffset)
